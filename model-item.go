c3e51c56d30e39cf234ef2c1137304a67357f5d1
YamiOdymel <yamiodymel@gmail.com>
2020-12-05 22:24:26 +0800

    修正 GetItemsList 的 requirement

--- model-item.go
+++ model-item.go
@@ -196,9 +196,9 @@ type DeleteVariationResponse struct {
 
 type GetItemsListRequest struct {
 	// Specifies the starting entry of data to return in the current call. Default is 0. if data is more than one page, the offset can be some entry to start next call.
-	PaginationOffset int `json:"pagination_offset,omitempty"`
+	PaginationOffset int `json:"pagination_offset"`
 	// If many items are available to retrieve, you may need to call GetItemsList multiple times to retrieve all the data. Each result set is returned as a page of entries. Use the Pagination filters to control the maximum number of entries (<= 100) to retrieve per page (i.e., per call), the offset number to start next call. This integer value is usUed to specify the maximum number of entries to return in a single ""page"" of data.
-	PaginationEntriesPerPage int `json:"pagination_entries_per_page,omitempty"`
+	PaginationEntriesPerPage int `json:"pagination_entries_per_page"`
 	// The update_time_from and update_time_to fields specify a date range for retrieving orders (based on the item update time). The update_time_from field is the starting date range. The maximum date range that may be specified with the update_time_from and update_time_to fields is 15 days.
 	UpdateTimeFrom int `json:"update_time_from,omitempty"`
 	// The update_time_from and update_time_to fields specify a date range for retrieving orders (based on the item update time). The update_time_to field is the ending date range. The maximum date range that may be specified with the update_time_from and update_time_to fields is 15 days.
